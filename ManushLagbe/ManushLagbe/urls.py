26c788fd29f256ec9c283d8034ce3567d27bec07
Ziad Hossain <51024876+ziad7933@users.noreply.github.com>
2019-07-19 23:25:10 +0600

    Add files via upload

--- ManushLagbe/ManushLagbe/urls.py
+++ ManushLagbe/ManushLagbe/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', include('signup.urls')),
     path('signin/', include('signin.urls')),
-
+    path('accounts/', include('allauth.urls')),
 ]
