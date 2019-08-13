f6e39792fff048ea8fae41dff5e25e9506b244d4
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-14 01:26:53 +0600

    urls added for connection of homepage, aboutus page and register page

---
+++ Project/Work/ManushLagbe (updated)/Manushlagbecore/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomePage.as_view(), name = 'home'),
+    path('aboutus/', views.AboutPage.as_view(), name = 'aboutus'),
+    path('signup/', views.RegisterPage.as_view(), name = 'register'),
+    
+
+] 
