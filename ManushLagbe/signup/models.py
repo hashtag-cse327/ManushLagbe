092924479c921b0260522924cf353ac8e0a40b84
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-14 02:29:06 +0600

    Delete models.py

--- ManushLagbe/signup/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Registeration(models.Model):
-	name = models.CharField(max_length = 200)
-	email = models.CharField(max_length = 200)
-	phone = models.CharField(max_length = 11)
-	password = models.CharField(max_length = 100)
