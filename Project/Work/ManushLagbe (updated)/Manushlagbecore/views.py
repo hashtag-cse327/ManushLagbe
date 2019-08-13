5805aa654fa2b28cf7441f90f0dc90180feeeba9
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-14 01:25:53 +0600

    htmls of index, aboutus and register added

--- Project/Work/ManushLagbe (updated)/Manushlagbecore/views.py
+++ Project/Work/ManushLagbe (updated)/Manushlagbecore/views.py
@@ -1,7 +1,21 @@
+from django.urls import path
+from . import views
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class HomePage(TemplateView):
+	template_name = 'index.html'
+
+class AboutPage(TemplateView):
+	template_name = 'aboutus.html'
+
+class RegisterPage(TemplateView):
+	template_name = 'register.html'
+
 def login(request):
   return render(request, 'login.html')
 
