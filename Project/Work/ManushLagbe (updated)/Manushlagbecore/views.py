8d768bdb5e368fb6c19bc0f8ae4c279a78bd1457
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-14 07:42:06 +0600

    Update views.py

--- Project/Work/ManushLagbe (updated)/Manushlagbecore/views.py
+++ Project/Work/ManushLagbe (updated)/Manushlagbecore/views.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
 
@@ -13,12 +11,3 @@ class HomePage(TemplateView):
 class AboutPage(TemplateView):
 	template_name = 'aboutus.html'
 
-class RegisterPage(TemplateView):
-	template_name = 'register.html'
-
-def login(request):
-  return render(request, 'login.html')
-
-@login_required
-def home(request):
-  return render(request, 'home.html')
