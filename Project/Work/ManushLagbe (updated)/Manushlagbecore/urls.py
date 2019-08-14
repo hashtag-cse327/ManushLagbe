8b057e31985c546eacadc2dc0ea062e4b2758752
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-14 07:42:32 +0600

    Update urls.py

--- Project/Work/ManushLagbe (updated)/Manushlagbecore/urls.py
+++ Project/Work/ManushLagbe (updated)/Manushlagbecore/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name = 'home'),
     path('aboutus/', views.AboutPage.as_view(), name = 'aboutus'),
-    path('signup/', views.RegisterPage.as_view(), name = 'register'),
-    
-
 ] 
