6b98d0c9fc03b7f60f532c2361d390ce7bdad983
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 04:08:47 +0600

    models updated(taking input and saving in db)

--- Project/Work/ManushLagbe (updated)/volunteer/models.py
+++ Project/Work/ManushLagbe (updated)/volunteer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Volunteer(models.Model):
+	name = models.CharField(max_length=200)
+	mob = models.CharField(max_length=20)
+	address = models.CharField(max_length=500)
+	gender = models.CharField(max_length=20)
+	age = models.CharField(max_length=20)
+	price = models.IntegerField()
+
+	def __str__(self):
+		return self.name
