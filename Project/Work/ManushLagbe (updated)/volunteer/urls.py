deee0329929b8369188787d2d94bcd49b6826d12
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-18 16:08:58 +0600

    urls updated (service provider)

--- Project/Work/ManushLagbe (updated)/volunteer/urls.py
+++ Project/Work/ManushLagbe (updated)/volunteer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [    
     path('volunteer-service/', views.VolunteerView.as_view(), name='volunteer'),   
+    path('volunteer-serviceprovider/', views.SvolunteerView.as_view(), name='svolunteer'),   
 ]
