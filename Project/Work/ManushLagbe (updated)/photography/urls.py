075bde08940335640db64c1bedcc395ee827d182
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-18 16:06:38 +0600

    urls updated for service provider

--- Project/Work/ManushLagbe (updated)/photography/urls.py
+++ Project/Work/ManushLagbe (updated)/photography/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [    
     path('photography-service/', views.PhotoView.as_view(), name='photo'),   
+    path('photography-serviceprovider/', views.SphotoView.as_view(), name='sphoto'),   
 ]
