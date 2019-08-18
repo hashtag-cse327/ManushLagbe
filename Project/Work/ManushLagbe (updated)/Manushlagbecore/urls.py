fcc16de87cd8937ff6ad3fc41e4ae23c27baf1b0
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-18 15:57:03 +0600

    urls updated for service provider

--- Project/Work/ManushLagbe (updated)/Manushlagbecore/urls.py
+++ Project/Work/ManushLagbe (updated)/Manushlagbecore/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name = 'home'),
     path('aboutus/', views.AboutPage.as_view(), name = 'aboutus'),
+    path('service-provider-home/', views.ShomePage.as_view(), name = 'provider'),
 ] 
