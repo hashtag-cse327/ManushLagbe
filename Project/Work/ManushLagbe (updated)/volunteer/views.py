38cab2c70d0e1d0e71eee1118ebcc29ec5c0784c
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 04:06:40 +0600

    views updated (taking input and saving in database)

--- Project/Work/ManushLagbe (updated)/volunteer/views.py
+++ Project/Work/ManushLagbe (updated)/volunteer/views.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Volunteer
 
 # Create your views here.
 
@@ -9,3 +12,19 @@ class VolunteerView(TemplateView):
 
 class SvolunteerView(TemplateView):
 	template_name = 'svolunteer.html'
+
+def volunteer_form_submission(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']
+	gender = request.POST['gender']
+	age = request.POST['age']
+	price = request.POST['price']
+
+	volunteer = Volunteer(name=name,mob=mob,
+		address=address,gender=gender,age=age,price=price)
+
+	volunteer.save()
+
+	return render(request, 'sindex.html')
