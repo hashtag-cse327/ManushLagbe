1391468a813debdb49404ebb713ca1bb07daed96
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-18 16:16:33 +0600

    urls updated (html file included)

--- Project/Work/ManushLagbe (updated)/mehedi/urls.py
+++ Project/Work/ManushLagbe (updated)/mehedi/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [    
-    path('mehedi-service/', views.MehediView.as_view(), name='mehedi'),   
+    path('mehndi-service/', views.MehediView.as_view(), name='mehedi'),
+    path('mehndi-serviceprovider/', views.SmehediView.as_view(), name='smehedi'),   
 ]
