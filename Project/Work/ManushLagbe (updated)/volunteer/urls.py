b36594c2632b7cb426e83885c54f101edfc2415d
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 04:07:20 +0600

    urls updated (taking input and saving in database)

--- Project/Work/ManushLagbe (updated)/volunteer/urls.py
+++ Project/Work/ManushLagbe (updated)/volunteer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [    
     path('volunteer-service/', views.VolunteerView.as_view(), name='volunteer'),   
     path('volunteer-serviceprovider/', views.SvolunteerView.as_view(), name='svolunteer'),   
+    path('volunteer_form_submission/', views.volunteer_form_submission, name='volunteer_form_submission'),   
 ]
