5b0c234d02547eccf78ef63c19308fe4835281d1
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-19 22:42:09 +0600

    register the tables

--- Project/Work/ManushLagbe (updated)/mehedi/admin.py
+++ Project/Work/ManushLagbe (updated)/mehedi/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Mehedi
+from .models import Mehedi, Mehedi_Customer
 # Register your models here.
 
 admin.site.register(Mehedi)
+admin.site.register(Mehedi_Customer)
+
