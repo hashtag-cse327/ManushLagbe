7bcde13c105516772015a82b005292970bc44eff
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 19:08:33 +0600

    customer table added in volunteer

--- Project/Work/ManushLagbe (updated)/volunteer/models.py
+++ Project/Work/ManushLagbe (updated)/volunteer/models.py
@@ -11,3 +11,14 @@ class Volunteer(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Customer_volunteer(models.Model):
+	name = models.CharField(max_length=200)
+	mob = models.CharField(max_length=20)
+	address = models.CharField(max_length=500)
+	event_type = models.CharField(max_length=20)
+	volunteer_number = models.CharField(max_length=20)
+	hours = models.IntegerField()
+
+	def __str__(self):
+		return self.name
