9259b5427bfea5f3027f71973b3665741cc0342e
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-20 01:09:42 +0600

    Update urls.py

--- Project/Work/ManushLagbe (updated)/volunteer/urls.py
+++ Project/Work/ManushLagbe (updated)/volunteer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('volunteer-serviceprovider/', views.SvolunteerView.as_view(), name='svolunteer'),   
     path('volunteer_form_submission/', views.volunteer_form_submission, name='volunteer_form_submission'),   
     path('volunteer_search/', views.volunteer_search, name='volunteer_search'),   
-    path('volunteer_list/', views.Volunteer_list, name='Volunteer_list'),   
+    path('volunteer_list/', views.volunteer_search, name='Volunteer_list'),   
 ]
  
