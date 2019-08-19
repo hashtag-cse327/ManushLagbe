c3ebafbc1a70fbc1f7ba772115280888224394e2
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 19:09:27 +0600

    customer_volunteer registered

--- Project/Work/ManushLagbe (updated)/volunteer/admin.py
+++ Project/Work/ManushLagbe (updated)/volunteer/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Volunteer
+from .models import Customer_volunteer
 # Register your models here.
 
 admin.site.register(Volunteer)
+admin.site.register(Customer_volunteer)
+ 
