33e3582a67e33b11bd706057ef39f99becb0b083
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-19 15:45:00 +0600

    created tables for database

--- Project/Work/ManushLagbe (updated)/driver/views.py
+++ Project/Work/ManushLagbe (updated)/driver/views.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Driver
+
 
 # Create your views here.
 
@@ -9,3 +13,24 @@ class DriverView(TemplateView):
 
 class SdriverView(TemplateView):
 	template_name = 'sdriver.html'
+
+def driver_form_submission(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']
+	gender = request.POST['gender']
+	age = request.POST['age']
+	transmission = request.POST['transmission']
+	auto_price = request.POST['auto_price']
+	manual_price = request.POST['manual_price']
+
+
+	driver = Driver(name=name,mob=mob,
+		address=address,gender=gender,age=age, transmission = transmission, auto_price = auto_price, manual_price =manual_price)
+
+	driver.save()
+
+	return render(request, 'sindex.html')
+
+	
