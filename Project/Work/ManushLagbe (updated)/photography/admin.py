f90e9e8e49845193b71796f4a8cb7db7ea6fbe8d
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-19 18:49:35 +0600

    database included

--- Project/Work/ManushLagbe (updated)/photography/admin.py
+++ Project/Work/ManushLagbe (updated)/photography/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Photographer,Customer_Photographer
 
 # Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Customer_Photographer)
+
