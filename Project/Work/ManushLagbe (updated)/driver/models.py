3a7b9bdf42ef0090fcc7072c3d442184d4d2fc10
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-19 15:47:35 +0600

    created table characteristics

--- Project/Work/ManushLagbe (updated)/driver/models.py
+++ Project/Work/ManushLagbe (updated)/driver/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Driver(models.Model):
+	name = models.CharField(max_length=200)
+	mob = models.CharField(max_length=20)
+	address = models.CharField(max_length=500)
+	gender = models.CharField(max_length=20)
+	age = models.CharField(max_length=20)
+	transmission = models.CharField(max_length=20)
+	auto_price = models.IntegerField()
+	manual_price = models.IntegerField()
+
+
+	def __str__(self):
+		return self.name
