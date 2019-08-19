2c876616d1cb1d4b46e99ed032fdd859385c41f5
Ziad Hossain <51024876+ziad7933@users.noreply.github.com>
2019-08-19 15:39:05 +0600

    Defining the table characteristics

--- Project/Work/ManushLagbe (updated)/mehedi/models.py
+++ Project/Work/ManushLagbe (updated)/mehedi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Mehedi(models.Model):
+	name = models.CharField(max_length=200)
+	mob = models.CharField(max_length=20)
+	address = models.CharField(max_length=500)
+	gender = models.CharField(max_length=20)
+	age = models.CharField(max_length=20)
+	price_ohos = models.IntegerField()
+	price_ohbs = models.IntegerField()
+	price_thos = models.IntegerField()
+	price_thbs = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
 # Create your models here.
