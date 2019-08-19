1786d2b815a0395f1ba7730d30446e965ac5e0d0
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-19 18:51:40 +0600

    create database table

--- Project/Work/ManushLagbe (updated)/photography/models.py
+++ Project/Work/ManushLagbe (updated)/photography/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Photographer(models.Model):
+	name = models.CharField(max_length=200)
+	mob = models.CharField(max_length=20)
+	address = models.CharField(max_length=500)
+	gender = models.CharField(max_length=20)
+	age = models.CharField(max_length=20)
+
+	photographer_type = models.CharField(max_length=20)
+	small_event_charge = models.IntegerField()
+	large_event_charge = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+class Customer_Photographer(models.Model):
+	name = models.CharField(max_length=200)
+	mob  = models.CharField(max_length=20)
+	address = models.CharField(max_length=200)
+
+	event_type = models.CharField(max_length=50)
+	number_of_photographer = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
