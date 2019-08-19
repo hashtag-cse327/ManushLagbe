8c6df3c13ec812080253a88b8f4937510732e9de
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-19 19:06:02 +0600

    views updated(inputs added)

--- Project/Work/ManushLagbe (updated)/volunteer/views.py
+++ Project/Work/ManushLagbe (updated)/volunteer/views.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Volunteer
+from .models import Customer_volunteer
 
 # Create your views here.
 
@@ -12,6 +13,9 @@ class VolunteerView(TemplateView):
 
 class SvolunteerView(TemplateView):
 	template_name = 'svolunteer.html'
+
+class Volunteer_list(TemplateView):
+	template_name = 'v_list.html'
 
 def volunteer_form_submission(request):
 	print("Your data is saved!!!")
@@ -28,3 +32,19 @@ def volunteer_form_submission(request):
 	volunteer.save()
 
 	return render(request, 'sindex.html')
+
+def volunteer_search(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']
+	event = request.POST['event']
+	number = request.POST['number']
+	hours = request.POST['hours']
+
+	customer_volunteer = Customer_volunteer(name=name,mob=mob,
+		address=address,event_type=event,volunteer_number=number,hours=hours)
+
+	customer_volunteer.save()
+
+	return render(request, 'v_list.html')
