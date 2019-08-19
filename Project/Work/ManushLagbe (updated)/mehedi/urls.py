351c5daac6fe1f1326369c17cb7cb9192f9d705b
Ziad Hossain <51024876+ziad7933@users.noreply.github.com>
2019-08-19 15:38:10 +0600

    Added pathway for a request to locate

--- Project/Work/ManushLagbe (updated)/mehedi/urls.py
+++ Project/Work/ManushLagbe (updated)/mehedi/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [    
     path('mehndi-service/', views.MehediView.as_view(), name='mehedi'),
-    path('mehndi-serviceprovider/', views.SmehediView.as_view(), name='smehedi'),   
+    path('mehndi-serviceprovider/', views.SmehediView.as_view(), name='smehedi'),  
+    path('mehedi_form_submission/', views.mehedi_form_submission, name='mehedi_form_submission'), 
 ]
