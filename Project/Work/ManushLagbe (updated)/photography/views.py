25efb528a8ed8c0024fbe9beacd019b5417d2c14
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-19 18:57:42 +0600

    added customer and provider database form

--- Project/Work/ManushLagbe (updated)/photography/views.py
+++ Project/Work/ManushLagbe (updated)/photography/views.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Photographer
+from .models import Customer_Photographer
 
 # Create your views here.
 
@@ -9,3 +13,46 @@ class PhotoView(TemplateView):
 
 class SphotoView(TemplateView):
 	template_name = 'sphotography.html'
+
+class Photographer_list(TemplateView):
+	template_name = 'p_list.html'
+
+
+def photographer_form_submission(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']
+	gender = request.POST['gender']
+	age = request.POST['age']
+
+	ptype = request.POST['ptype']
+	se_charge = request.POST['se_charge']
+	le_charge = request.POST['le_charge']
+
+
+	photographer = Photographer(name=name,mob=mob,
+		address=address,gender=gender,age=age,photographer_type=ptype,
+		small_event_charge=se_charge,large_event_charge=le_charge)
+
+	photographer.save()
+
+	return render(request, 'sindex.html')
+
+def customer_photographer_form_submission(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']
+
+	etype = request.POST['etype']
+	photographer_number = request.POST['photographer_number']
+
+	customer_photographer = Customer_Photographer(name=name,mob=mob,address=address,
+			event_type=etype, number_of_photographer = photographer_number)
+
+	customer_photographer.save()
+
+	return render(request, 'p_list.html')
+
+
