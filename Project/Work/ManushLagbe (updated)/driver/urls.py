9b6a7d2bd107ad55876fdc8be81d15b75c654cfe
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-19 15:48:40 +0600

    added pathway

--- Project/Work/ManushLagbe (updated)/driver/urls.py
+++ Project/Work/ManushLagbe (updated)/driver/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [    
     path('driver-service/', views.DriverView.as_view(), name='driver'),   
-    path('driver-serviceprovider/', views.SdriverView.as_view(), name='sdriver'),   
+    path('driver-serviceprovider/', views.SdriverView.as_view(), name='sdriver'),  
+    path('driver_form_submission/', views.driver_form_submission, name='driver_form_submission'),   
 ]
