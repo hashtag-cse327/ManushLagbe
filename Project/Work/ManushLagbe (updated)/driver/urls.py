7598abed3b0c6d1847d77ce031c78727fcf0d7d9
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 13:30:36 +0600

    added pathway for url request for drivers

--- Project/Work/ManushLagbe (updated)/driver/urls.py
+++ Project/Work/ManushLagbe (updated)/driver/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from . import views
 
+
+#here all the pathways are mentioned which leads to the desired page
+#when a request is made
 urlpatterns = [    
     path('driver-service/', views.DriverView.as_view(), name='driver'),   
     path('driver-serviceprovider/', views.SdriverView.as_view(), name='sdriver'),  
-    path('driver_form_submission/', views.driver_form_submission, name='driver_form_submission'),   
+    path('driver_form_submission/', views.driver_form_submission, name='driver_form_submission'), 
+    path('driver_customer_form_submission/', views.driver_customer_form_submission, name='driver_customer_form_submission'),
+    path('driver_list/', views.driver_customer_form_submission, name='driver_list'),  
 ]
