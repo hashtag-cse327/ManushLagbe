41cd177e440daa7433e27de157d8c06ead4b80be
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 21:44:47 +0600

    create authentication view for register

--- Project/Work/ManushLagbe (updated)/signup/views.py
+++ Project/Work/ManushLagbe (updated)/signup/views.py
@@ -1,10 +1,23 @@
-from django.urls import path
-from . import views
+from django.contrib.auth import authenticate, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 from django.views.generic import TemplateView
 
 
 # Create your views here.
+User = get_user_model()
+def register_page(request):                     #defines function  register_page
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
 
-class RegisterPage(TemplateView):
-	template_name = 'register.html'
-
+    return render(request, "register.html", context)
