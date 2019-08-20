36e711c200e03ee9742dbc02f51058cd704d014b
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 21:46:04 +0600

    path update according to new views function

--- Project/Work/ManushLagbe (updated)/signup/urls.py
+++ Project/Work/ManushLagbe (updated)/signup/urls.py
@@ -1,6 +1,7 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.RegisterPage.as_view(), name = 'register'),
+    path('signup/', views.register_page, name = 'register'),
 ] 
