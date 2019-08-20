906d7bc97b9811c6fd81348ebfbc78002e59f98b
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 21:54:46 +0600

    authenticate user log in created

--- Project/Work/ManushLagbe (updated)/signin/views.py
+++ Project/Work/ManushLagbe (updated)/signin/views.py
@@ -1,12 +1,43 @@
 from django.urls import path
 from . import views
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
-def login(request):
-  return render(request, 'login.html')
+from .forms import LoginForm, LoginUsernameForm  #import classes from forms.py 
 
-@login_required
-def home(request):
-  return render(request, 'home.html') 
+
+def login_page(request):
+	username = LoginUsernameForm(request.POST or None)
+	form = LoginForm(request.POST or None)
+	context = {
+		"username": username,
+		"form": form,
+	}
+	print("User logged in")
+	#print(request.user.is_authenticated())
+	if form.is_valid():
+		print(form.cleaned_data)		#cleaned_data check validation data and print show the data in teminal
+
+	if username.is_valid():
+		print(username.cleaned_data)	#cleaned_data check validation data and print show the data in teminal
+
+		Username = username.cleaned_data.get("username")
+		Password = form.cleaned_data.get("password")
+		user = authenticate(request, username=Username, password=Password)
+		print(user)
+		#print(request.user.is_authenticated())
+		if user is not None:
+			#print(request.user.is_authenticated())
+			login(request, user)
+			#Redirect to a success page.
+			#context['form'] = LoginForm()
+			return redirect("/")
+		else:
+			#Return an 'invalid login' error message.
+			print ("Errror")
+			
+	return render(request, "login.html", context)
