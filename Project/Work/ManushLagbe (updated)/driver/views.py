e15e45fb70a0fdefa9617468e59a1b796d20934a
Ziad Hossain <51024876+ziad7933@users.noreply.github.com>
2019-08-20 13:23:14 +0600

    Added the customer page and list view for driver

--- Project/Work/ManushLagbe (updated)/driver/views.py
+++ Project/Work/ManushLagbe (updated)/driver/views.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Driver
+from .models import Driver_Customer
 
 
 # Create your views here.
@@ -32,5 +33,26 @@ def driver_form_submission(request):
 	driver.save()
 
 	return render(request, 'sindex.html')
+
+def driver_customer_form_submission(request):
+	print("Your data is saved!!!")
+	name = request.POST['name']
+	mob = request.POST['mob']
+	address = request.POST['address']					#defining the tables and making a view to show as a list
+	transmission = request.POST['transmission']
+	hours = request.POST['hours']
+
+	driver_customer = Driver_Customer(name=name, mob=mob, address=address, transmission=transmission, 
+						hours=hours)
+
+	driver_customer.save()
+
+	drivers= Driver.objects.all()
+	context={
+		"object_list": drivers,                           #fetches data from database of driver and shows as a list in the html
+	}
+	return render(request,'dsp.html',context)
+
+	return render(request, 'dsp.html')
 
 	
