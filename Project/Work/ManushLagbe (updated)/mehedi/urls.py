a26c3dd72103f6b16143985b9aa760ea04d32626
fatematujzahurarumman <50975166+fatematujzahurarumman@users.noreply.github.com>
2019-08-20 12:33:18 +0600

    added pathway for mehedi artistlist

--- Project/Work/ManushLagbe (updated)/mehedi/urls.py
+++ Project/Work/ManushLagbe (updated)/mehedi/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
 
+
+#pathways to the required pages
 urlpatterns = [    
     path('mehndi-service/', views.MehediView.as_view(), name='mehedi'),
     path('mehndi-serviceprovider/', views.SmehediView.as_view(), name='smehedi'),  
     path('mehedi_form_submission/', views.mehedi_form_submission, name='mehedi_form_submission'),
     path('mehedi_customer_form_submission/', views.mehedi_customer_form_submission, name='mehedi_customer_form_submission'),
-    path('mehedi_artist_list/', views.Mehedi_artist_list, name='mehedi_artist_list'),
+    path('mehedi_artist_list/', views.mehedi_customer_form_submission, name='mehedi_artist_list'),
 ]
