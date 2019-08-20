d872452616f9d31213772d0ef88a1c0bcf5b091d
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 13:26:12 +0600

    registered the classes added in views

--- Project/Work/ManushLagbe (updated)/driver/admin.py
+++ Project/Work/ManushLagbe (updated)/driver/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Driver
+from .models import Driver_Customer
 
 # Register your models here.
 admin.site.register(Driver)
+admin.site.register(Driver_Customer)
