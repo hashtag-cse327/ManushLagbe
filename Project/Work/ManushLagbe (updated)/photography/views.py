4966d61585e4e82e64357785d435a1c453213ef7
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-20 12:03:56 +0600

    updated fetching part

--- Project/Work/ManushLagbe (updated)/photography/views.py
+++ Project/Work/ManushLagbe (updated)/photography/views.py
@@ -9,21 +9,21 @@ from .models import Customer_Photographer
 # Create your views here.
 
 class PhotoView(TemplateView):
-	template_name = 'photography.html'
+	template_name = 'photography.html' #loading template photography.html file
 
 class SphotoView(TemplateView):
-	template_name = 'sphotography.html'
+	template_name = 'sphotography.html' #loading the html file of service provider's photographer file
 
-class Photographer_list(TemplateView):
-	template_name = 'p_list.html'
+#class Photographer_list(TemplateView):
+#	template_name = 'p_list.html'
 
 
 def photographer_form_submission(request):
 	print("Your data is saved!!!")
 	name = request.POST['name']
 	mob = request.POST['mob']
-	address = request.POST['address']
-	gender = request.POST['gender']
+	address = request.POST['address']		#data from form is saved in the variables
+	gender = request.POST['gender']			
 	age = request.POST['age']
 
 	ptype = request.POST['ptype']
@@ -35,7 +35,7 @@ def photographer_form_submission(request):
 		address=address,gender=gender,age=age,photographer_type=ptype,
 		small_event_charge=se_charge,large_event_charge=le_charge)
 
-	photographer.save()
+	photographer.save() #data saved
 
 	return render(request, 'sindex.html')
 
@@ -52,6 +52,14 @@ def customer_photographer_form_submission(request):
 			event_type=etype, number_of_photographer = photographer_number)
 
 	customer_photographer.save()
+
+	photographers = Photographer.objects.all()
+	context={
+		"object_list": photographers,
+	}
+	return render(request, "p_list.html", context)
+
+
 
 	return render(request, 'p_list.html')
 
