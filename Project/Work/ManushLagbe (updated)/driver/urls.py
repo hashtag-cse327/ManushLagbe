34582abcfb90a145ca2856d80a5814d917080af9
Horraira <35287199+Horraira@users.noreply.github.com>
2019-08-24 17:25:41 +0600

    urls updated

--- Project/Work/ManushLagbe (updated)/driver/urls.py
+++ Project/Work/ManushLagbe (updated)/driver/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from . import views
 
-
 #here all the pathways are mentioned which leads to the desired page
 #when a request is made
 urlpatterns = [    
     path('driver-service/', views.DriverView.as_view(), name='driver'),   
-    path('driver-serviceprovider/', views.SdriverView.as_view(), name='sdriver'),  
+    path('driver-serviceprovider/', views.SdriverView, name='sdriver'),  
     path('driver_form_submission/', views.driver_form_submission, name='driver_form_submission'), 
-    path('driver_customer_form_submission/', views.driver_customer_form_submission, name='driver_customer_form_submission'),
+   # path('driver_customer_form_submission/', views.driver_customer_form_submission, name='driver_customer_form_submission'),
+    path('driver_customer_form_submission/', views.search, name='driver_search'),      #selected search
     path('driver_list/', views.driver_customer_form_submission, name='driver_list'),  
 ]
