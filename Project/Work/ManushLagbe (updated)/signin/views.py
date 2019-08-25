98ec2b3090905c7c6b562beaddac41c6f4124c53
Iftiaz Ahmed <35066363+Iftiaz-Ahmed@users.noreply.github.com>
2019-08-25 20:02:51 +0600

    login issues solved

--- Project/Work/ManushLagbe (updated)/signin/views.py
+++ Project/Work/ManushLagbe (updated)/signin/views.py
@@ -19,25 +19,52 @@ def login_page(request):
 	}
 	print("User logged in")
 	#print(request.user.is_authenticated())
-	if form.is_valid():
-		print(form.cleaned_data)		#cleaned_data check validation data and print show the data in teminal
+	if 'customer' in request.POST:
+		if form.is_valid():
+			print(form.cleaned_data)		#cleaned_data check validation data and print show the data in teminal
 
-	if username.is_valid():
-		print(username.cleaned_data)	#cleaned_data check validation data and print show the data in teminal
+		if username.is_valid():
+			print(username.cleaned_data)	#cleaned_data check validation data and print show the data in teminal
 
-		Username = username.cleaned_data.get("username")
-		Password = form.cleaned_data.get("password")
-		user = authenticate(request, username=Username, password=Password)
-		print(user)
-		#print(request.user.is_authenticated())
-		if user is not None:
+			Username = username.cleaned_data.get("username")
+			Password = form.cleaned_data.get("password")
+			user = authenticate(request, username=Username, password=Password)
+			print(user)
 			#print(request.user.is_authenticated())
-			login(request, user)
-			#Redirect to a success page.
-			#context['form'] = LoginForm()
-			return redirect("/")
-		else:
-			#Return an 'invalid login' error message.
-			print ("Errror")
-			
+			if user is not None:
+				#print(request.user.is_authenticated())
+				if not user.is_staff:
+					login(request, user)
+				else:
+					print("Error cause not customer")
+				#Redirect to a success page.
+				#context['form'] = LoginForm()
+				return redirect("home")
+			else:
+				#Return an 'invalid login' error message.
+				print ("Errror")
+	elif 'serviceProvider' in request.POST:
+		if form.is_valid():
+			print(form.cleaned_data)		#cleaned_data check validation data and print show the data in teminal
+
+		if username.is_valid():
+			print(username.cleaned_data)	#cleaned_data check validation data and print show the data in teminal
+
+			Username = username.cleaned_data.get("username")
+			Password = form.cleaned_data.get("password")
+			user = authenticate(request, username=Username, password=Password)
+			print(user)
+			#print(request.user.is_authenticated())
+			if user is not None:
+				#print(request.user.is_authenticated())
+				if user.is_staff:
+					login(request, user)                            #if is_staff not true that means he is not a service provider
+				else:
+					print("Error cause not serviceProvider")
+				#Redirect to a success page.
+				#context['form'] = LoginForm()
+				return redirect("provider")
+			else:
+				#Return an 'invalid login' error message.
+				print ("Errror")
 	return render(request, "login.html", context)
