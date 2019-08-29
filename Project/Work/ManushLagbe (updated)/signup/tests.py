a3bb93918aae6e156e6083017dee7c3f43cf0890
Ziad Hossain <51024876+ziad7933@users.noreply.github.com>
2019-08-29 11:50:12 +0600

    Update tests.py

--- Project/Work/ManushLagbe (updated)/signup/tests.py
+++ Project/Work/ManushLagbe (updated)/signup/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from .forms import RegisterForm
 
 # Create your tests here.
+
+class RegisterForm_test(TestCase):
+
+	def test_registerForm(self):
+
+		 # test invalid data
+
+		 invalid_data = {
+		 	"username":"user@test.com",
+		 	"email"   :"horraira",
+		 	"password": "secret",
+     	    "confirm": "not secret"
+		 }
+
+		 form = RegisterForm(data=invalid_data)
+		 form.is_valid()
+		 self.assertTrue(form.errors)
+
+		 # test valid data
+
+		 valid_data = {
+		 	"username":"user@test.com",
+		 	"email"   :"horraira",
+		 	"password": "secret",
+     	    "confirm": "not secret"
+		 }
+
+		 form = RegisterForm(data=invalid_data)
+		 form.is_valid()
+		 self.assertFalse(form.errors)
